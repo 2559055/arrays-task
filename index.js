71d163e43f3b0121c5a8977f67f6347ece3fed95
2559055 <mohamed.hussein@student.moringaschoo.com>
2023-10-06 12:16:01 -0700

    task of arrays

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+
+var userName = ["Milo", "Otis", "Garfield"];
+var myData = userName[1];
+console.log(userName);
+
+let cats = []; 
+
+beforeEach(() => {
+  
+  const catName = "zaki"; 
+  cats.push(catName); 
+});
+
+
